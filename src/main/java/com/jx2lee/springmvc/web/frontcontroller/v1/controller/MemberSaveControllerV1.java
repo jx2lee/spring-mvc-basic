f1af53998995e7b02d14a4fff4c5a4822bcd73ee
jx2lee <jaejun.lee.1991@gmail.com>
2021-07-20 12:30:43 +0900

    섹션 4. MVC 프레임워크 만들기: 프론트 컨트롤러 도입 - v1
    * FrontControllerServletV1: 서블릿
    * MemberFormController, MemberSaveController, MemberListController: ControllerV1 인터페이스로 구현한 컨트롤러 (클래스)
    * 모든 요청은 FrontControllerServletV1 이 받고 매핑 정보(controllerMap)를 이용해 알맞는 컨트롤러 호출하는 형태
    * jsp 에서 상대경로를 사용하는데, 프로젝트 성격에 맞게 절대/상대경로를 사용하면 좋다. (복잡하지 않고 일정한 URL 패턴을 가진 경우 상대, 왠만하면 절대경로를 사용하자)

---
+++ src/main/java/com/jx2lee/springmvc/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,32 @@
+package com.jx2lee.springmvc.web.frontcontroller.v1.controller;
+
+import com.jx2lee.springmvc.domain.member.Member;
+import com.jx2lee.springmvc.domain.member.MemberRepository;
+import com.jx2lee.springmvc.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        // 데이터 보관 to Model
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
