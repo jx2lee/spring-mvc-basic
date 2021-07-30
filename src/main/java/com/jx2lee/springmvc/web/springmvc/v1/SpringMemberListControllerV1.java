bd019897f58d11d42c867e94077058e6096d09e1
jx2lee <jaejun.lee.1991@gmail.com>
2021-07-30 15:06:06 +0900

    섹션 5. 스프링 MVC - 구조 이해: 스프링 MVC - 시작하기
    * @Controller & @RequestMapping 을 통한 리팩토링
    * paramMap 사용없이 HttpServletRequest 로 파라미터 값을 가져올 수 있다.
    * url pattern 꼭 확인!

---
+++ src/main/java/com/jx2lee/springmvc/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,24 @@
+package com.jx2lee.springmvc.web.springmvc.v1;
+
+import com.jx2lee.springmvc.domain.member.Member;
+import com.jx2lee.springmvc.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
